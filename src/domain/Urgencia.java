51cb12b9787298a561a1832db4ff5f6250728c2e
KennyMls <delgadokenny4@gmail.com>
2022-08-04 20:50:49 -0500

    Urgencia

--- src/domain/Urgencia.java
+++ src/domain/Urgencia.java
@@ -1,4 +1,44 @@
 package domain;
 
+import java.time.LocalDateTime;
+
 public class Urgencia {
+    private long numero;
+    private LocalDateTime fechaLlegada;
+    private LocalDateTime fechaAsignacion;
+    private Persona medicoClas;
+    private String sintomas;
+    private byte triage;
+
+    public Urgencia(long numero, LocalDateTime fechaLlegada, Persona medicoClas, String sintomas, byte triage) {
+        this.numero = numero;
+        this.fechaLlegada = fechaLlegada;
+        this.medicoClas = medicoClas;
+        this.sintomas = sintomas;
+        this.triage = triage;
+    }
+
+    public void setNumero(long numero) {
+        this.numero = numero;
+    }
+
+    public void setFechaLlegada(LocalDateTime fechaLlegada) {
+        this.fechaLlegada = fechaLlegada;
+    }
+
+    public void setFechaAsignacion(LocalDateTime fechaAsignacion) {
+        this.fechaAsignacion = fechaAsignacion;
+    }
+
+    public void setMedicoClas(Persona medicoClas) {
+        this.medicoClas = medicoClas;
+    }
+
+    public void setSintomas(String sintomas) {
+        this.sintomas = sintomas;
+    }
+
+    public void setTriage(byte triage) {
+        this.triage = triage;
+    }
 }
